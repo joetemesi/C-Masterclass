1dae463fba4d5346b49e9ce154cb3fc9b5eeb210
joetemesi <joemunyendo@gmail.com>
2024-03-05 13:50:33 +0300

    Variable declaration and initialization in C#

---
+++ TodoList/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What do you want to do today?\n");
+
+Console.WriteLine("[S]see all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+//Variable declarations and Initialization
+string userInput = "A";
+Console.WriteLine(userInput);
+userInput = "ABC\n";
+Console.WriteLine(userInput);
+
+int myNumber = 1;
+Console.WriteLine(myNumber);
+ myNumber = 123;
+Console.WriteLine(myNumber);
+
+Console.ReadKey();
+//Console.WriteLine("Hello, World!");
+//Console.ReadKey();
+
