70d8813fadc175b921f6b3811e8772436d68a399
joetemesi <joemunyendo@gmail.com>
2024-03-06 21:10:18 +0300

    Add Bool logic

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("What do you want to do today?\n");
 
@@ -7,15 +9,22 @@ Console.WriteLine("[A]dd a TODO");
 Console.WriteLine("[R]emove a TODO");
 Console.WriteLine("[E]xit");
 //Variable declarations and Initialization
-string userInput = "A";
-Console.WriteLine(userInput);
-userInput = "ABC\n";
-Console.WriteLine(userInput);
+//string userInput = "A";
+//Console.WriteLine(userInput);
+//userInput = "ABC\n";
+//Console.WriteLine(userInput);
 
-int myNumber = 1;
-Console.WriteLine(myNumber);
- myNumber = 123;
-Console.WriteLine(myNumber);
+
+
+//int myNumber = 1;
+//Console.WriteLine(myNumber);
+// myNumber = 123;
+//Console.WriteLine(myNumber);
+
+var userInput = Console.ReadLine(); ///implicit declaration
+
+bool isUserInputABC = userInput == "ABC";
+
 
 Console.ReadKey();
 //Console.WriteLine("Hello, World!");
