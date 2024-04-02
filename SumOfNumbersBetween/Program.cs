809c62392f1e101b6d7770b37c6e825515ba52be
joetemesi <joemunyendo@gmail.com>
2024-04-02 12:28:35 +0300

    Add logic for calculation of sum of numbers between two intergers

---
+++ SumOfNumbersBetween/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Console.WriteLine("Hello, World!");
+
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        // Call the method within the Main method
+        int sum = Exercise.CalculateSumOfNumbersBetween(firstNumber, secondNumber);
+        Console.WriteLine("Sum of numbers between " + firstNumber + " and " + secondNumber + " is: " + sum);
+        Console.ReadKey();
+    }
+}
+public class Exercise
+{
+    public static int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+    {
+        //your code goes here
+        int sum = 0;
+        if (firstNumber > lastNumber)
+        {
+            sum = 0;
+            return sum;
+        }
+        else if (firstNumber == lastNumber)
+        {
+            sum = firstNumber;
+            return sum;
+        }
+        else
+        {
+            int currentNumber = firstNumber;
+            while (currentNumber <= lastNumber)
+            {
+                sum += currentNumber;
+                currentNumber++;
+            }
+            return sum;
+        }
+    }
+}
+
