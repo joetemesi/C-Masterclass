54188d92a1dbbe8f66ed3268e5e1890ed01baa52
joetemesi <joemunyendo@gmail.com>
2024-04-05 15:39:49 +0300

    Add method to check if an item exists in an array

---
+++ IsWordPresentInCollection/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static bool IsWordPresentInCollection(string[] words, string wordToBeChecked)
+        {
+            //your code goes here
+            int arrayLength = words.Length;
+            bool isPresent = false;
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                string myWord = words[i];
+
+                isPresent = wordToBeChecked == myWord;
+
+                if (isPresent)
+                {
+                    break;
+                }
+
+            }
+            return isPresent;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "apple", "banana", "orange" };
+            string wordToBeChecked = "banana";
+
+            bool result = Exercise.IsWordPresentInCollection(words, wordToBeChecked);
+            Console.WriteLine($"Is the word {wordToBeChecked} present in the collection? " + result);
+            Console.ReadKey();
+        }
+    }
+}
+
